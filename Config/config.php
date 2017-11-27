a8af9927e4495730ac16bba8dc1cf02c676e087c
danielsGrietins <daniels.grietins@gmail.com>
2017-11-27 21:36:58 +0200

    Map configs

--- Config/config.php
+++ Config/config.php
@@ -2,13 +2,14 @@
 
 return [
     // some projects don't have text-block, that's why we can disable it
-    'text-block'  => true,
+    'text-block'   => true,
 
     // some projects don't have map in contacts page, that's why we can disable it
-    'map'         => true,
+    'map'          => true,
+    'maps_api_key' => '',
 
     // some projects don't have information block, that's why we can disable it
-    'information' => [
+    'information'  => [
         'enabled'       => true, // this variable is responsible whether to use this block or not.
 
         // variables below are responsible for what information you want show in information block
